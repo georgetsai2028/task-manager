518c7946d717b2008e5d0e8b426700f584ea3876
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-15 19:09:24 -0700

    testing out vanilla extract

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
+import { flexCenter } from "./App.css";
 
 function App() {
-  let name: string;
-  let age: number;
-  let isStudent: boolean;
-  let hobbies: string[];
-  let role: [number, string];
-  const [count, setCount] = useState(0);
-
-  type Person = {
-    name: string;
-    age?: number;
-  };
-
-  let bob: Person = {
-    name: "Bob",
-    age: 29,
-  };
-
-  return (
-    <div>
-      <p> Hello {bob.name}! </p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  );
+  return <div className={flexCenter}>abc</div>;
 }
 
 export default App;
