3012187c8a0b58db448d173c7dc589b704335a28
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-15 22:13:12 -0700

    adding todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { flexCenter } from "./App.css";
-import { dark } from "./vars.css";
+import { dark, light } from "./vars.css";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 
 function App() {
   return (
     <div className={dark}>
-      <div className={flexCenter}>abc</div>;
+      <div className={flexCenter}>
+        <TodoList />
+        <TodoForm />
+        <TodoItem />
+      </div>
+      ;
     </div>
   );
 }
